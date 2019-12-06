f52e7d854210e8c6178089849102af9e98dff9eb
Austin Trow <austintrow@gmail.com>
2019-12-06 10:55:13 -0500

    part 1 done and quite a bit of part 2 :)

--- 5-1/solution.js
+++ 5-1/solution.js
@@ -1,32 +1,76 @@
+const INPUTS = require('./input');
+
 function intcodeReaderV2(code, ...inputs) {
   let i = 0;
   while (i < code.length) {
     let [instruction, parameters] = parseInstruction(code[i]);
-
+    // console.log(
+    //   ` at ${i}: action ${instruction} with parameters ${parameters}`
+    // );
     switch (instruction) {
       case '99':
         console.log('program complete');
         return;
-      case '01':
-        let [noun, verb, target] = code.slice(i + 1, i + 4);
-        code[target] = code[noun] + code[verb];
+      case '01': {
+        noun = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        verb = parameters[1] ? code[i + 2] : code[code[i + 2]];
+        target = code[i + 3];
+        // console.log(`adding ${noun}, ${verb} and placing it at ${target}`);
+        code[target] = noun + verb;
         i += 4;
         break;
-      case '02':
-        [noun, verb, target] = code.slice(i + 1, i + 4);
-        code[target] = code[noun] * code[verb];
+      }
+      case '02': {
+        noun = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        verb = parameters[1] ? code[i + 2] : code[code[i + 2]];
+        target = code[i + 3];
+        // console.log(`multiplying ${noun}, ${verb} and placing it at ${target}`);
+        code[target] = noun * verb;
         i += 4;
         break;
-      case '03':
-        // target = code[i + 1];
-        code[code[i + 1]] = inputs[0];
+      }
+      case '03': {
+        let target = code[i + 1];
+        code[target] = inputs[0];
         i += 2;
         break;
-      case '04':
-        // target = code[i + 1];
-        console.log(code[code[i + 1]]);
+      }
+      case '04': {
+        let output = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        console.log(`Line ${i}: ${output}`);
         i += 2;
         break;
+      }
+      case '05': {
+        let isTrue = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        let target = parameters[1] ? code[i + 2] : code[code[i + 2]];
+        if (isTrue) i = target;
+        else i += 3;
+        break;
+      }
+      case '06': {
+        let isTrue = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        let target = parameters[1] ? code[i + 2] : code[code[i + 2]];
+        if (!isTrue) i = target;
+        else i += 3;
+        break;
+      }
+      case '07': {
+        let first = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        let second = parameters[1] ? code[i + 2] : code[code[i + 2]];
+        let target = code[i + 3];
+        code[target] = first < second ? 1 : 0;
+        i += 4;
+        break;
+      }
+      case '08': {
+        let first = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        let second = parameters[1] ? code[i + 2] : code[code[i + 2]];
+        let target = code[i + 3];
+        code[target] = first === second ? 1 : 0;
+        i += 4;
+        break;
+      }
       default:
         throw new Error(
           `You goofed. Invalid intCode ${code[i]} at location ${i}`
@@ -36,6 +80,7 @@ function intcodeReaderV2(code, ...inputs) {
 }
 
 function parseInstruction(rawInstruction) {
+  // console.log(rawInstruction);
   let stringInstruction = String(rawInstruction);
   let instruction = '';
   let parameters = [];
@@ -47,9 +92,13 @@ function parseInstruction(rawInstruction) {
   if (stringInstruction.length > 2) {
     parameters = stringInstruction
       .slice(0, stringInstruction.length - 2)
-      .split('');
+      .split('')
+      .reverse()
+      .map(char => Number(char));
   }
   return [instruction, parameters];
 }
 
 intcodeReaderV2([3, 0, 4, 0, 99], 15);
+
+intcodeReaderV2(INPUTS, 1);
