cc14cf5fa0045e0c4dcc307c85bcd4c612370f44
Austin Trow <austintrow@gmail.com>
2019-12-06 13:37:25 -0500

    working!

--- 6-2/solution.js
+++ 6-2/solution.js
@@ -1,2 +1,35 @@
 const INPUTS = require('../6-1/inputs');
 const { buildOrbitGraph } = require('../6-1/solution');
+
+function transferCounter(orbitMap) {
+  const orbitGraph = buildOrbitGraph(orbitMap);
+  let start = orbitGraph['YOU'].parent;
+  let end = orbitGraph['SAN'].parent;
+
+  let totalTransfers = 0;
+  let startPath = [];
+  let endPath = [];
+
+  let currentNode = orbitGraph[start];
+  while (currentNode.parent) {
+    totalTransfers++;
+    startPath.unshift(currentNode.parent);
+    currentNode = orbitGraph[currentNode.parent];
+  }
+
+  currentNode = orbitGraph[end];
+  while (currentNode.parent) {
+    totalTransfers++;
+    endPath.unshift(currentNode.parent);
+    currentNode = orbitGraph[currentNode.parent];
+  }
+
+  console.log(totalTransfers);
+
+  for (let i = 1; i < startPath.length; i++) {
+    if (startPath[i] === endPath[i]) totalTransfers -= 2;
+    else return totalTransfers;
+  }
+}
+
+console.log(transferCounter(INPUTS));
