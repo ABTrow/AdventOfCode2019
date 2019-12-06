4412a21069d3ea668b532aa2d6c65e78a7ba8c48
Austin Trow <austintrow@gmail.com>
2019-12-06 11:21:27 -0500

    dryed out the code a bit

--- 5-1/solution.js
+++ 5-1/solution.js
@@ -3,71 +3,65 @@ const INPUTS = require('./input');
 function intcodeReaderV2(code, ...inputs) {
   let i = 0;
   while (i < code.length) {
-    let [instruction, parameters] = parseInstruction(code[i]);
+    let [instruction, parameterModes] = parseInstruction(code[i]);
+
+    let param1 = parameterModes[0] ? code[i + 1] : code[code[i + 1]];
+    let param2 = parameterModes[1] ? code[i + 2] : code[code[i + 2]];
+    let param3 = parameterModes[2] ? code[i + 3] : code[code[i + 3]];
+
     // console.log(
-    //   ` at ${i}: action ${instruction} with parameters ${parameters}`
+    //   ` at ${i}: action ${instruction} with parameter modes ${parameterModes}`
     // );
     switch (instruction) {
       case '99':
         console.log('program complete');
         return;
       case '01': {
-        noun = parameters[0] ? code[i + 1] : code[code[i + 1]];
-        verb = parameters[1] ? code[i + 2] : code[code[i + 2]];
-        target = code[i + 3];
-        // console.log(`adding ${noun}, ${verb} and placing it at ${target}`);
-        code[target] = noun + verb;
+        let target = code[i + 3];
+        // console.log(`adding ${param1}, ${param2} and placing it at ${target}`);
+        code[target] = param1 + param2;
         i += 4;
         break;
       }
       case '02': {
-        noun = parameters[0] ? code[i + 1] : code[code[i + 1]];
-        verb = parameters[1] ? code[i + 2] : code[code[i + 2]];
-        target = code[i + 3];
-        // console.log(`multiplying ${noun}, ${verb} and placing it at ${target}`);
-        code[target] = noun * verb;
+        let target = code[i + 3];
+        // console.log(`multiplying ${param1}, ${param2} and placing it at ${target}`);
+        code[target] = param1 * param2;
         i += 4;
         break;
       }
       case '03': {
-        let target = code[i + 1];
+        target = code[i + 1];
+        // console.log(`placing ${inputs[0]} at ${target}`);
         code[target] = inputs[0];
         i += 2;
         break;
       }
       case '04': {
-        let output = parameters[0] ? code[i + 1] : code[code[i + 1]];
+        let output = param1;
         console.log(`Line ${i}: ${output}`);
         i += 2;
         break;
       }
       case '05': {
-        let isTrue = parameters[0] ? code[i + 1] : code[code[i + 1]];
-        let target = parameters[1] ? code[i + 2] : code[code[i + 2]];
-        if (isTrue) i = target;
+        if (param1) i = param2;
         else i += 3;
         break;
       }
       case '06': {
-        let isTrue = parameters[0] ? code[i + 1] : code[code[i + 1]];
-        let target = parameters[1] ? code[i + 2] : code[code[i + 2]];
-        if (!isTrue) i = target;
+        if (!param1) i = param2;
         else i += 3;
         break;
       }
       case '07': {
-        let first = parameters[0] ? code[i + 1] : code[code[i + 1]];
-        let second = parameters[1] ? code[i + 2] : code[code[i + 2]];
         let target = code[i + 3];
-        code[target] = first < second ? 1 : 0;
+        code[target] = param1 < param2 ? 1 : 0;
         i += 4;
         break;
       }
       case '08': {
-        let first = parameters[0] ? code[i + 1] : code[code[i + 1]];
-        let second = parameters[1] ? code[i + 2] : code[code[i + 2]];
         let target = code[i + 3];
-        code[target] = first === second ? 1 : 0;
+        code[target] = param1 === param2 ? 1 : 0;
         i += 4;
         break;
       }
@@ -99,6 +93,4 @@ function parseInstruction(rawInstruction) {
   return [instruction, parameters];
 }
 
-intcodeReaderV2([3, 0, 4, 0, 99], 15);
-
-intcodeReaderV2(INPUTS, 1);
+intcodeReaderV2(INPUTS, 5);
