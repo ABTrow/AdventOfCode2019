09b69f1fd7a89e08ceda2ffc204d81163f2366bc
Austin Trow <austintrow@gmail.com>
2019-12-08 01:12:32 -0500

    ezpz on 8-2

--- 8-2/solution.js
+++ 8-2/solution.js
@@ -1,9 +1,21 @@
 const INPUT = require('../8-1/input');
 
 function imageCompressor(data) {
-  let finalImage = Array(6);
-  finalImage = finalImage.map(row => Array(25).fill(null));
-  console.log(finalImage);
+  let finalImage = Array(6).fill('x');
+  finalImage = finalImage.map(row => Array(20).fill(undefined));
+
+  for (let i = 0; i < 150; i++) {
+    for (let j = 0; j < data.length; j += 150) {
+      if (data[i + j] !== '2') {
+        let row = Math.floor(i / 25);
+        let column = i % 25;
+        finalImage[row][column] = data[i + j] === '1' ? '@' : ' ';
+        break;
+      }
+    }
+  }
+
+  return finalImage.map(row => row.join(''));
 }
 
-imageCompressor(INPUT);
+console.log(imageCompressor(INPUT));
