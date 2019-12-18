b6897cedf89200dd951caaac6691b2cd47c310fb
Austin Trow <austintrow@gmail.com>
2019-12-18 18:38:53 -0500

    optimized for part 2

--- 16-1/solution.js
+++ 16-1/solution.js
@@ -1,40 +1,39 @@
 const INPUT = require('./input');
 
-// const INPUT = `80871224585914546619083218645595`;
+// const INPUT = `03036732577212944063491565474664`;
 
 // const INPUT = `12345678`;
 
-let inputArray = INPUT.split('').map(el => Number(el));
+const offset = Number(INPUT.slice(0, 7));
 
-function cleanupTransmission(input) {
+const longInput = INPUT.repeat(10000);
+
+console.longInput;
+
+let inputArray = longInput.split('').map(el => Number(el));
+
+function cleanupTransmission(input, offset) {
   let output = [...input];
-  let basePattern = [0, 1, 0, -1];
 
-  output = output.map((outputEl, outputIndex) => {
-    let newTotal = 0;
-    let patternIndex = 0;
-    let patternCount = 1;
-    input.forEach(inputEl => {
-      if (patternCount === outputIndex + 1) {
-        patternCount = 0;
-        patternIndex = (patternIndex + 1) % basePattern.length;
-      }
-      patternCount++;
-      newTotal += inputEl * basePattern[patternIndex];
-    });
-    return Math.abs(newTotal) % 10;
-  });
+  for (let o = output.length - 2; o >= offset; o--) {
+    output[o] = (output[o + 1] + output[o]) % 10;
+  }
   return output;
 }
 
-function multiCleanup(inputArray, phases) {
+function multiCleanup(inputArray, offset, phases) {
   let currentPhase = 0;
   let currentTransmission = [...inputArray];
   while (currentPhase < phases) {
-    currentTransmission = cleanupTransmission(currentTransmission);
+    currentTransmission = cleanupTransmission(currentTransmission, offset);
     currentPhase++;
+    console.log(`phase ${currentPhase} completed`);
   }
   return currentTransmission;
 }
 
-console.log(multiCleanup(INPUT, 100).join(''));
+console.log(
+  multiCleanup(inputArray, offset, 100)
+    .join('')
+    .slice(offset, offset + 8)
+);
